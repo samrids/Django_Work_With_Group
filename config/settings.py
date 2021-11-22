100036476cd04a7925761188ce1d4a7ea80ca6b3
MESA.D <samrids@gmail.com>
2021-11-22 22:39:47 +0700

    Fixed key

--- config/settings.py
+++ config/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-1j3+yf4#ji8dwsc5$)7g&a*ggh^spj%p67or5jxd!rzir=v7c*'
+SECRET_KEY = '<<KEY>>'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
